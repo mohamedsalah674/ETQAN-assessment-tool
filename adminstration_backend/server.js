effde7e34f518f1597d15d1d79fb7c42987e4065
mohamedsalah112 <ms2000.mohamedsalah@gmail.com>
2023-06-09 14:04:42 +0300

    push

--- adminstration_backend/server.js
+++ adminstration_backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 
 
 
+
 // const course_created_consumer = require("./events/consumer/course-created-event")
 // const course_updated_consumer = require("./events/consumer/course-updated-event")
 // const course_deleted_consumer = require("./events/consumer/course-deleted-event")
